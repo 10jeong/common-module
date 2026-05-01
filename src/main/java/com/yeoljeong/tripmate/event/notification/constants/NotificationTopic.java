f8d8205836cdb5e951c9e0912a4b3bade8285439
yujin <yujin09219@naver.com>
2026-05-01 11:10:59 +0900

    fix : 이벤트 객체 수정 및 이벤트 해시 수정

--- src/main/java/com/yeoljeong/tripmate/event/notification/constants/NotificationTopic.java
+++
@@ -1,6 +0,0 @@
-package com.yeoljeong.tripmate.event.notification.constants;
-
-public final class NotificationTopic {
-
-  public static final String SEND = "notification.send";
-}
--- src/main/java/com/yeoljeong/tripmate/event/notification/payload/NotificationPayload.java
+++
@@ -1,8 +0,0 @@
-package com.yeoljeong.tripmate.event.notification.payload;
-
-import com.yeoljeong.tripmate.event.notification.constants.NotificationType;
-
-public interface NotificationPayload {
-
-  NotificationType getType();
-}
