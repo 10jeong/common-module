493c96ce20a8a2afb09e37b078e22bf966180c85
Yoonho Cha <0119hoya@naver.com>
2026-05-01 12:14:35 +0900

    feat: 카프카 토픽 추가

---
+++ src/main/java/com/yeoljeong/tripmate/event/enums/PaymentTopic.java
@@ -0,0 +1,17 @@
+package com.yeoljeong.tripmate.event.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum PaymentTopic {
+
+    /*
+     * 토픽에 대한 컨벤션은 "domain.resource.active"로 합니다. (~가 ~를 한다.)
+     */
+
+    PAYMENT_COMPLETED_TOPIC("payment.completed"),
+    PAYMENT_FAILED_TOPIC("payment.failed"),
+    ;
+    private final String name;
+
+}
